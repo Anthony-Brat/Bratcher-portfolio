317fc8ff759cfc52f0c10a603bd953348a6c80b8
Anthony Bratcher <tonybrat74@gmail.com>
2020-12-13 14:53:53 -0800

    deleted variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
+  // const [date, setDate] = useState(null);
   // useEffect(() => {
   //   async function getDate() {
   //     const res = await fetch('/api/date');
